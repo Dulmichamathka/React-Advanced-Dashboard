75d96b8f7f92ff5ff0d207544a1a40f560e6d8d4
Dulmi Chamathka <chamathkawijesekara23@gmail.com>
2024-10-13 19:17:05 +0530

    second commit

--- my-dashboard/src/App.js
+++ my-dashboard/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import {BrowserRouter,Route, Routes} from 'react-router-dom'
+import {BrowserRouter,Route, Routes} from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Dashboard from './pages/Dashboard';
+import Header from './pages/Dashboard/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
            <Route path='/' exact={true} element = {<Dashboard/>}/>
       </Routes>
